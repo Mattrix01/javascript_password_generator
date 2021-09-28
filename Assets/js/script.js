b43c0b1733848693005f3da612ce815dfdb7758c
Mattrix01 <emjay.knight@gmail.com>
2021-09-28 23:35:18 +0100

    user input taken into variables for function WIP

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,26 @@ var generateBtn = document.querySelector("#generate");
 
 // PLEASE ADD ALL YOUR CODE BELOW -
 function generatePassword() {
+  const characters = prompt(
+    "What is the length of your password? Please choose between 8 and 128 characters"
+  );
+  const charactersInt = parseInt(characters);
+  if (charactersInt >= 8 && charactersInt <= 128) {
+    passwordLength = charactersInt;
+  } else {
+    window.alert("Invalid number, please try again within 8 to 128 characters");
+  }
+  //question 2 lowercase included?
+  const lowerCase = prompt("Would you like to include lowercase characters?");
+  //question 3 uppercase uncluded?
+  const upperCase = prompt("Would you like to include uppercase characters?");
+  //question 4 include numerical?
+  const numericals = prompt("Would you like to include numerical characters?");
+  //question 5 include special characters?
+  const specialCharacters = prompt(
+    "Would you like to include special characters?"
+  );
+
   const lower = [
     "a",
     "b",
@@ -67,20 +87,24 @@ function generatePassword() {
 
   console.log(combinedCriteria);
 
-  passwordLength = 15;
+  // passwordLength = 15;
 
   const randomElement =
     combinedCriteria[Math.floor(Math.random() * combinedCriteria.length)];
 
   console.log(randomElement);
-  /*
-  myPassword = "";
-  for (let i=0; i < passwordLength; i++) {
-    myPassword = myPassword + combinedCriteria[Math.floor(Math.random() * combinedCriteria.length)];
-  }
 
+  // I want to define a new variable to my password as empty string
+  // enter into for loop which will iterate untill the index has hit that password length.
+  // for each iteration, the string my password will have a new character added onto it.
+  myPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    myPassword =
+      myPassword +
+      combinedCriteria[Math.floor(Math.random() * combinedCriteria.length)]; // return a single random character everytime
+  }
+  // once the loop completed the variable will be 15(password length)characters long.
   console.log(myPassword);
-*/
 }
 
 //  Write password to the #password input -
@@ -93,33 +117,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/*  Write password to the #password input - 
-taking value of generate password, 
-whatever comes out of generate password function above gets put into the password variable, 
-putting the generated password value into the password box on the html side so users can see it.
-need to use some sort of alert, prompt, confirm boxes apprently */
-
-// function generatePassword() {
-// //   //question 1 character length?
-// //   let characters = prompt(
-// //     "What is the length of your password? Please choose between 8 and 128 characters"
-// //   );
-// //   let characters = parseInt(characters);
-// //   if (characters >= 8 && characters <= 128) {
-// //     window.alert = "Input accepted!";
-// //   } else {
-// //     window.alert("Invalid number, please try again within 8 to 128 characters");
-// //     return;
-// //   }
-// //   //question 2 lowercase included?
-// //   const lowerCase = prompt("Would you like to include lowercase characters?");
-// //   //question 3 uppercase uncluded?
-// //   const upperCase = prompt("Would you like to include uppercase characters?");
-// //   //question 4 include numerical?
-// //   const numericals = prompt("Would you like to include numerical characters?");
-// //   //question 5 include special characters?
-// //   const specialCharacters = prompt(
-// //     "Would you like to include special characters?"
-// //   );
-// // }
